ea360c94bbb13b85010f9d076ab7fbb140429b78
Frank Anderson <frob@249517.no-reply.drupal.org>
2015-12-15 11:36:16 -0800

    initial commit

---
+++ twelve-piles_edge.js
@@ -0,0 +1,376 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+   fonts['open-sans, sans-serif']='<script src=\"http://use.edgefonts.net/open-sans:n7,i7,n8,i8,i4,n3,i3,n4,n6,i6:all.js\"></script>';
+
+
+var resources = [
+];
+var symbols = {
+"stage": {
+   version: "1.5.0",
+   minimumCompatibleVersion: "1.5.0",
+   build: "1.5.0.217",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+         dom: [
+         {
+            id:'border',
+            type:'rect',
+            rect:['0px','0px','298px','238px','auto','auto'],
+            fill:["rgba(252,210,0,0.00)"],
+            stroke:[1,"rgba(204,204,204,1.00)","solid"]
+         },
+         {
+            id:'Rectangle',
+            type:'rect',
+            rect:['36px','36px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy3',
+            type:'rect',
+            rect:['207px','90px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy4',
+            type:'rect',
+            rect:['109px','176px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy5',
+            type:'rect',
+            rect:['94px','156px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy6',
+            type:'rect',
+            rect:['130px','145px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy7',
+            type:'rect',
+            rect:['25px','57px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy8',
+            type:'rect',
+            rect:['71px','48px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy10',
+            type:'rect',
+            rect:['109px','135px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy11',
+            type:'rect',
+            rect:['215px','77px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy12',
+            type:'rect',
+            rect:['207px','48px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy13',
+            type:'rect',
+            rect:['186px','69px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'RectangleCopy9',
+            type:'rect',
+            rect:['52px','77px','40px','40px','auto','auto'],
+            fill:["rgba(252,210,0,1.00)"],
+            stroke:[1,"rgba(255,255,255,1.00)","solid"]
+         },
+         {
+            id:'button-outline',
+            type:'rect',
+            rect:['109px','21px','82px','34px','auto','auto'],
+            cursor:['pointer'],
+            borderRadius:["20px 20px","20px 20px","20px 20px","20px 20px"],
+            opacity:1,
+            fill:["rgba(255,255,255,1.00)"],
+            stroke:[1,"rgb(0, 129, 198)","solid"]
+         },
+         {
+            id:'text-play',
+            type:'text',
+            rect:['110px','25px','80px','25px','auto','auto'],
+            text:"PLAY<br>",
+            align:"center",
+            font:['open-sans, sans-serif',20,"rgba(0,129,198,1)","300","none","normal"]
+         },
+         {
+            id:'button-coverCopy',
+            type:'rect',
+            rect:['109px','22px','76px','33px','auto','auto'],
+            cursor:['pointer'],
+            fill:["rgba(255,255,255,1)"],
+            stroke:[1,"rgb(0, 129, 198)","solid"]
+         },
+         {
+            id:'number',
+            type:'text',
+            rect:['110px','25px','80px','25px','auto','auto'],
+            text:"1",
+            align:"center",
+            font:['open-sans, sans-serif',20,"rgba(0,129,198,1)","300","none","normal"]
+         },
+         {
+            id:'Label',
+            type:'text',
+            rect:['0px','204px','300px','25px','auto','auto'],
+            text:"3 groups of 4 squares",
+            align:"center",
+            font:['open-sans, sans-serif',14,"rgba(0,129,198,1)","300","none","normal"]
+         }],
+         symbolInstances: [
+
+         ]
+      },
+   states: {
+      "Base State": {
+         "${_RectangleCopy9}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["color", "border-color", 'rgb(255, 255, 255)'],
+            ["style", "left", '36px'],
+            ["style", "border-style", 'solid'],
+            ["style", "border-width", '1px'],
+            ["style", "top", '155px']
+         ],
+         "${_RectangleCopy4}": [
+            ["style", "top", '187px'],
+            ["color", "border-color", 'rgba(255,255,255,1.00)'],
+            ["style", "border-width", '1px'],
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '144px']
+         ],
+         "${_RectangleCopy12}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "top", '177px'],
+            ["style", "border-width", '1px'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '215px'],
+            ["color", "border-color", 'rgb(255, 255, 255)']
+         ],
+         "${_text-play}": [
+            ["style", "top", '25px'],
+            ["style", "display", 'block'],
+            ["style", "opacity", '1'],
+            ["style", "left", '110px'],
+            ["style", "height", '25px']
+         ],
+         "${_RectangleCopy13}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "top", '98px'],
+            ["style", "border-width", '1px'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '159px'],
+            ["color", "border-color", 'rgb(255, 255, 255)']
+         ],
+         "${_Rectangle}": [
+            ["style", "top", '77px'],
+            ["style", "border-width", '1px'],
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '57px'],
+            ["color", "border-color", 'rgba(255,255,255,1.00)']
+         ],
+         "${_RectangleCopy10}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["color", "border-color", 'rgb(255, 255, 255)'],
+            ["style", "top", '134px'],
+            ["style", "border-width", '1px'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '73px']
+         ],
+         "${_Label}": [
+            ["style", "top", '204px'],
+            ["style", "font-size", '14px'],
+            ["style", "height", '25px'],
+            ["style", "display", 'block'],
+            ["style", "opacity", '0'],
+            ["style", "left", '0px'],
+            ["style", "width", '300px']
+         ],
+         "${_RectangleCopy6}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "top", '119px'],
+            ["color", "border-color", 'rgb(255, 255, 255)'],
+            ["style", "left", '144px'],
+            ["style", "border-style", 'solid'],
+            ["style", "border-width", '1px']
+         ],
+         "${_RectangleCopy3}": [
+            ["style", "top", '113px'],
+            ["style", "border-width", '1px'],
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '228px'],
+            ["color", "border-color", 'rgba(255,255,255,1.00)']
+         ],
+         "${_number}": [
+            ["style", "top", '8px'],
+            ["style", "height", '25px'],
+            ["style", "display", 'block'],
+            ["style", "opacity", '1'],
+            ["style", "left", '9px'],
+            ["style", "width", '21px']
+         ],
+         "${_RectangleCopy7}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["color", "border-color", 'rgb(255, 255, 255)'],
+            ["style", "left", '9px'],
+            ["style", "border-style", 'solid'],
+            ["style", "border-width", '1px'],
+            ["style", "top", '134px']
+         ],
+         "${_RectangleCopy8}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["color", "border-color", 'rgb(255, 255, 255)'],
+            ["style", "left", '194px'],
+            ["style", "border-style", 'solid'],
+            ["style", "border-width", '1px'],
+            ["style", "top", '145px']
+         ],
+         "${_RectangleCopy5}": [
+            ["style", "top", '156px'],
+            ["color", "border-color", 'rgba(255,255,255,1.00)'],
+            ["style", "border-width", '1px'],
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '94px']
+         ],
+         "${_button-coverCopy}": [
+            ["style", "top", '22px'],
+            ["style", "display", 'block'],
+            ["style", "left", '109px'],
+            ["style", "height", '33px'],
+            ["style", "opacity", '0'],
+            ["style", "cursor", 'pointer'],
+            ["style", "width", '76px']
+         ],
+         "${_button-outline}": [
+            ["style", "top", '21px'],
+            ["color", "background-color", 'rgba(255,255,255,1)'],
+            ["style", "cursor", 'pointer'],
+            ["style", "display", 'block'],
+            ["style", "opacity", '1'],
+            ["style", "left", '109px'],
+            ["style", "width", '78px']
+         ],
+         "${_border}": [
+            ["color", "background-color", 'rgba(252,210,0,0.00)'],
+            ["style", "top", '0px'],
+            ["style", "border-width", '1px'],
+            ["style", "border-style", 'solid'],
+            ["style", "height", '238px'],
+            ["color", "border-color", 'rgba(204,204,204,1.00)'],
+            ["style", "left", '0px'],
+            ["style", "width", '298px']
+         ],
+         "${_Stage}": [
+            ["color", "background-color", 'rgba(255,255,255,1)'],
+            ["style", "overflow", 'hidden'],
+            ["style", "height", '240px'],
+            ["style", "width", '300px']
+         ],
+         "${_RectangleCopy11}": [
+            ["color", "background-color", 'rgba(252,210,0,1)'],
+            ["style", "top", '134px'],
+            ["style", "border-width", '1px'],
+            ["style", "border-style", 'solid'],
+            ["style", "left", '249px'],
+            ["color", "border-color", 'rgb(255, 255, 255)']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 1577,
+         autoPlay: false,
+         labels: {
+            "Start": 1500
+         },
+         timeline: [
+            { id: "eid45", tween: [ "style", "${_RectangleCopy11}", "left", '242px', { fromValue: '249px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid44", tween: [ "style", "${_RectangleCopy13}", "left", '213px', { fromValue: '159px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid29", tween: [ "style", "${_RectangleCopy9}", "left", '57px', { fromValue: '36px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid38", tween: [ "style", "${_RectangleCopy4}", "top", '145px', { fromValue: '187px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid56", tween: [ "style", "${_text-play}", "opacity", '0', { fromValue: '1'}], position: 0, duration: 250 },
+            { id: "eid58", tween: [ "style", "${_text-play}", "opacity", '0', { fromValue: '0'}], position: 1500, duration: 0 },
+            { id: "eid31", tween: [ "style", "${_RectangleCopy10}", "left", '142px', { fromValue: '73px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid35", tween: [ "style", "${_RectangleCopy8}", "left", '76px', { fromValue: '194px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid46", tween: [ "style", "${_RectangleCopy11}", "top", '50px', { fromValue: '134px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid30", tween: [ "style", "${_RectangleCopy9}", "top", '79px', { fromValue: '155px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid32", tween: [ "style", "${_RectangleCopy10}", "top", '104px', { fromValue: '134px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid57", tween: [ "style", "${_button-outline}", "opacity", '0', { fromValue: '1'}], position: 0, duration: 250 },
+            { id: "eid59", tween: [ "style", "${_button-outline}", "opacity", '0', { fromValue: '0'}], position: 1500, duration: 0 },
+            { id: "eid47", tween: [ "style", "${_RectangleCopy3}", "top", '63px', { fromValue: '113px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid73", tween: [ "style", "${_RectangleCopy5}", "left", '127px', { fromValue: '94px'}], position: 0, duration: 1500 },
+            { id: "eid42", tween: [ "style", "${_Rectangle}", "left", '41px', { fromValue: '57px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid36", tween: [ "style", "${_RectangleCopy8}", "top", '50px', { fromValue: '145px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid71", tween: [ "style", "${_number}", "opacity", '1', { fromValue: '1'}], position: 1500, duration: 0 },
+            { id: "eid50", tween: [ "style", "${_RectangleCopy12}", "left", '234px', { fromValue: '215px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid43", tween: [ "style", "${_RectangleCopy13}", "top", '42px', { fromValue: '98px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid48", tween: [ "style", "${_RectangleCopy3}", "left", '234px', { fromValue: '228px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid72", tween: [ "style", "${_RectangleCopy5}", "top", '125px', { fromValue: '156px'}], position: 0, duration: 1500 },
+            { id: "eid40", tween: [ "style", "${_RectangleCopy6}", "top", '114px', { fromValue: '119px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid33", tween: [ "style", "${_RectangleCopy7}", "left", '30px', { fromValue: '9px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid34", tween: [ "style", "${_RectangleCopy7}", "top", '59px', { fromValue: '134px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid39", tween: [ "style", "${_RectangleCopy6}", "left", '163px', { fromValue: '144px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid37", tween: [ "style", "${_RectangleCopy4}", "left", '142px', { fromValue: '144px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid111", tween: [ "style", "${_Label}", "opacity", '1', { fromValue: '0'}], position: 1404, duration: 173 },
+            { id: "eid53", tween: [ "style", "${_button-coverCopy}", "opacity", '0', { fromValue: '0'}], position: 0, duration: 0 },
+            { id: "eid60", tween: [ "style", "${_button-coverCopy}", "opacity", '0', { fromValue: '0'}], position: 250, duration: 0 },
+            { id: "eid49", tween: [ "style", "${_RectangleCopy12}", "top", '21px', { fromValue: '177px'}], position: 0, duration: 1500, easing: "easeOutQuad" },
+            { id: "eid41", tween: [ "style", "${_Rectangle}", "top", '38px', { fromValue: '77px'}], position: 0, duration: 1500, easing: "easeOutQuad" }         ]
+      }
+   }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-192605612");
